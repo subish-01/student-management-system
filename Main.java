4d6ab9d87b284dedb40c8e03a9994573fffa9f3a
subish-01 <subishsingh7@gmail.com>
2026-08-26 20:46:47 +0530

    added student details into arraylist

--- Main.java
+++ Main.java
@@ -1,7 +1,20 @@
+
+import java.util.ArrayList;
+
 public class Main 
 {
     public static void main(String[] args)
     {
-        System.out.println("Student management system");
+        ArrayList<Student> list = new ArrayList<>();
+        Student st1 = new Student(1, "RaviKumar", 20, "CSE");
+        Student st2 = new Student(2, "Vishnu", 18, "IT");
+        list.add(st1);
+        list.add(st2);
+
+        for(Student s : list)
+        {
+            System.out.println(s);
+        }  
+
     }
 }
--- Student.java
+++ Student.java
@@ -1,14 +1,21 @@
-class Student 
+
+public class Student 
 {
     int id;
     String name;
     int age;
     String department;
-}
-Student(int id,String name,int age,String dept)
-{
-    this.id = id;
-    this.name = name;
-    this.age = age;
-    this.department = dept;
+
+    Student(int id,String name,int age,String dept)
+    {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.department = dept;
+    }
+    @Override
+    public String toString()
+    {
+        return "id :" + id + " Name :" + name + " Age : " + age + " Department : " + department;
+    }
 }
