fc0f16c54ddc87d1d780555931651dd1926144ed
subish-01 <subishsingh7@gmail.com>
2026-08-26 19:54:24 +0530

    created a blueprint for students

---
+++ Student.java
@@ -0,0 +1,14 @@
+class Student 
+{
+    int id;
+    String name;
+    int age;
+    String department;
+}
+Student(int id,String name,int age,String dept)
+{
+    this.id = id;
+    this.name = name;
+    this.age = age;
+    this.department = dept;
+}
