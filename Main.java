3bdda5795207e6db213793009c550018a440bba0
subish-01 <subishsingh7@gmail.com>
2026-09-05 21:50:48 +0530

    Got user input for one-student and stored in arraylist

--- Main.java
+++ Main.java
@@ -1,16 +1,24 @@
 
-import java.util.ArrayList;
+import java.util.*;
 
 public class Main 
 {
     public static void main(String[] args)
     {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter student id : ");
+        int id = scan.nextInt();
+        scan.nextLine();
+        System.out.print("Enter your name : ");
+        String name = scan.nextLine();
+        System.out.print("Enter your age : ");
+        int age = scan.nextInt();
+        scan.nextLine();
+        System.out.print("Enter your department : ");
+        String dept = scan.nextLine();
         ArrayList<Student> list = new ArrayList<>();
-        Student st1 = new Student(1, "RaviKumar", 20, "CSE");
-        Student st2 = new Student(2, "Vishnu", 18, "IT");
+        Student st1 = new Student(id,name,age,dept);
         list.add(st1);
-        list.add(st2);
-
         for(Student s : list)
         {
             System.out.println(s);
