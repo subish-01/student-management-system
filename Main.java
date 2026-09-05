2e326ca5e86d26f9d9825f8cdaac29df81e5a3d3
subish-01 <subishsingh7@gmail.com>
2026-09-05 22:19:17 +0530

    Added multiple student details using for-loop

--- Main.java
+++ Main.java
@@ -6,19 +6,26 @@ public class Main
     public static void main(String[] args)
     {
         Scanner scan = new Scanner(System.in);
-        System.out.print("Enter student id : ");
-        int id = scan.nextInt();
-        scan.nextLine();
-        System.out.print("Enter your name : ");
-        String name = scan.nextLine();
-        System.out.print("Enter your age : ");
-        int age = scan.nextInt();
-        scan.nextLine();
-        System.out.print("Enter your department : ");
-        String dept = scan.nextLine();
+        System.out.print("Enter the number of students to be added : ");
+        int repeat = scan.nextInt();
         ArrayList<Student> list = new ArrayList<>();
-        Student st1 = new Student(id,name,age,dept);
-        list.add(st1);
+        for(int i = 1; i <= repeat; i++)
+        {
+            System.out.println("Student " + i);
+            System.out.print("Enter student id : ");
+            int id = scan.nextInt();
+            scan.nextLine();
+            System.out.print("Enter student name : ");
+            String name = scan.nextLine();
+            System.out.print("Enter student age : ");
+            int age = scan.nextInt();
+            scan.nextLine();
+            System.out.print("Enter student department : ");
+            String dept = scan.nextLine();
+            Student st_i = new Student(id,name,age,dept);
+            list.add(st_i);
+            System.out.println();
+        }
         for(Student s : list)
         {
             System.out.println(s);
